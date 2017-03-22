e73b78c2daa5c19dac9f1e318c54602489d31eb8
Elizabeth Nicholson <elizabethnich19@gmail.com>
2017-03-22 03:29:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    # self.split('.!?').count('.!?')
+    #   if self.split('.!?')
+    #     self.count('.!?')
+    #   elsif (self.split('.') || self.split('!') || self.split('?'))
+    #     self.count('.')
+      # end
+    self.squeeze('.!?').count('.!?')
   end
 end
